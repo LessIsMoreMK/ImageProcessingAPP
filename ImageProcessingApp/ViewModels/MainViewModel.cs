f3e577d73d85d5542fef9f953cb43957dc02dd14
LessIsMoreMK <maciej8kz@gmail.com>
2020-11-25 22:42:20 +0100

    Open/open default file commands

--- ImageProcessingApp/ViewModels/MainViewModel.cs
+++ ImageProcessingApp/ViewModels/MainViewModel.cs
@@ -1,7 +1,72 @@
-﻿namespace ImageProcessingApp
-{
-    public class MainViewModel : BaseViewModel
-    {
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
 
+namespace ImageProcessingApp
+{
+    public class MainViewModel : Window
+    {
+        #region Fields
+        private string _ImagePath;
+        #endregion
+
+        #region Properties
+        public string ImagePath
+        {
+            get =>_ImagePath; 
+            set
+            {
+                _ImagePath = value;
+                OnPropertyChanged(() => ImagePath);
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoadImageCommand { get; set; }
+        public ICommand LoadDefaultImageCommand { get; set; }
+        public ICommand SaveImageCommand { get; set; }
+        public ICommand ConvertCommand { get; set; }
+        public ICommand ConvertAsyncCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            LoadImageCommand = new RelayCommand(LoadImage);
+            LoadDefaultImageCommand = new RelayCommand(LoadDefaultImage);
+            SaveImageCommand = new RelayCommand(SaveImage);
+
+            var t = new TreeViewItem()
+
+        }
+        #endregion
+
+        #region Methods
+
+        private void LoadImage()
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.DefaultExt = (".png");
+            open.Filter = "Pictures (*.jpg;*.gif;*.png)|*.jpg;*.gif;*.png";
+
+            if (open.ShowDialog() == true)
+                ImagePath = open.FileName;
+        }
+
+        private void LoadDefaultImage()
+        {
+            ImagePath = Path.Combine(Environment.CurrentDirectory, "../../Images/default.jpg");
+        }
+
+        private void SaveImage()
+        {
+
+        }
+        #endregion
     }
 }
