e95fcd678f9c308acf0adb762c29253895dd0c63
LessIsMoreMK <maciej8kz@gmail.com>
2020-11-27 16:07:26 +0100

    Save and display working time

--- ImageProcessingApp/ViewModels/MainViewModel.cs
+++ ImageProcessingApp/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Windows.Input;
 using System.Windows.Media;
 using ImageConverterLibrary;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
 
 namespace ImageProcessingApp
 {
@@ -12,6 +14,7 @@ namespace ImageProcessingApp
         #region Fields
         private string _ImagePath;
         private ImageSource _ImageResult;
+        private string _Time;
         #endregion
 
         #region Properties
@@ -31,6 +34,15 @@ namespace ImageProcessingApp
             {
                 _ImageResult = value;
                 OnPropertyChanged(() => ImageResult);
+            }
+        }
+        public string Time
+        {
+            get => _Time;
+            set
+            {
+                _Time = value;
+                OnPropertyChanged(() => Time);
             }
         }
         #endregion
@@ -74,17 +86,36 @@ namespace ImageProcessingApp
 
         private void SaveImage()
         {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.Title = "Save Image";
+            saveFileDialog1.ShowDialog();
 
+
+            if (saveFileDialog1.ShowDialog() ?? false)
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create((BitmapImage)_ImageResult));
+
+                using (var fileStream = new System.IO.FileStream(_ImagePath, System.IO.FileMode.Create))
+                {
+                    encoder.Save(fileStream);
+                }
+            }
         }
         private void ConvertRGB()
         {
+            var watch = Stopwatch.StartNew();
             ConvertRGB ConvertRGB = new ConvertRGB();
             ImageResult = ConvertRGB.ConvertRGBValue(_ImagePath);
+            Time = "Convert working time: " + watch.ElapsedMilliseconds + " ms";
         }
         private async void ConvertRGBAsync()
         {
+            var watch = Stopwatch.StartNew();
             ConvertRGB ConvertRGB = new ConvertRGB();
             ImageResult = await ConvertRGB.ConvertRGBValueAsync(_ImagePath);
+            Time = "Convert working time: " + watch.ElapsedMilliseconds + " ms";
         }
         #endregion
     }
